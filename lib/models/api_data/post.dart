2c7279399d2e13f26f3f8ae2e11476ee1cece5a9
augusstt06 <augusstt06@gmail.com>
2025-02-01 22:01:51 +0900

    add: response, request data model for post item

---
+++ lib/models/api_data/post.dart
@@ -0,0 +1,52 @@
+// 로아 개발 공홈 api
+// POST item 요청/응답 모델
+class PostItemRequest {
+  static const String sort = 'GRADE';
+  final int categoryCode;
+  final String itemName;
+  static const int pageNo = 0;
+  static const String sortCondition = 'ASC';
+
+  PostItemRequest({
+    required this.categoryCode,
+    required this.itemName,
+  });
+}
+
+class PostItemResponse {
+  final int pageNo;
+  final int pageSize;
+  final int totalCount;
+  final List<Item> items;
+
+  PostItemResponse({
+    required this.pageNo,
+    required this.pageSize,
+    required this.totalCount,
+    required this.items,
+  });
+}
+
+class Item {
+  final int id;
+  final String name;
+  final String grade;
+  final String icon;
+  final int bundleCount;
+  final int? tradeRemainCount;
+  final int yDayAvgPrice;
+  final int recentPrice;
+  final int currentMinPrice;
+
+  Item({
+    required this.id,
+    required this.name,
+    required this.grade,
+    required this.icon,
+    required this.bundleCount,
+    this.tradeRemainCount,
+    required this.yDayAvgPrice,
+    required this.recentPrice,
+    required this.currentMinPrice,
+  });
+}
