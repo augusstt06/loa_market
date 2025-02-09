79a2c28e9c1ab35f73b25a716f179a7a7a2eb2dc
augusstt06 <augusstt06@gmail.com>
2025-02-09 17:58:20 +0900

    fix: announce

--- lib/widgets/box/announce_box.dart
+++ lib/widgets/box/announce_box.dart
@@ -32,13 +32,21 @@ class AnnounceBox extends StatelessWidget {
               ),
               const Gap(15),
               CustomText(
-                title: '별칭으로도 아이템 검색이 가능합니다.',
+                title: '1. 별칭으로도 아이템 검색이 가능합니다.',
                 fontSize: 20,
+              ),
+              CustomText(
+                title: 'e.g. 숨결 검색 => 용암/빙하의 숨결,태양의 가호/축복/은총 리스트업.',
+                fontSize: 17,
               ),
               const Gap(10),
               CustomText(
-                title: 'e.g. 숨결 검색 => 용암/빙하의 숨결,태양의 가호/축복/은총 리스트업.',
+                title: '2. 현재 시세 그래프는 수요가 많은 아이템에 한해 제공됩니다. ',
                 fontSize: 20,
+              ),
+              CustomText(
+                title: 'e.g. 유각, 강화재료',
+                fontSize: 17,
               ),
             ],
           ),
